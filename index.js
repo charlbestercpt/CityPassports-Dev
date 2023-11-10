3c53762b505a1ba8326da740a017e5f85b8466b2
charlbestercpt <111444412+charlbestercpt@users.noreply.github.com>
2023-11-10 11:13:28 +0200

    Test2

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+const data = JSON.parse(localStorage.getItem("data"));
+
+//log experience variables
+let productOptionCode = data.bookableItems[0].productOptionCode;
+if (productOptionCode === undefined) {
+  productOptionCode = "No Product Option Code";
+}
+console.log(productOptionCode);
+
+const seasons = data.bookableItems[0].seasons;
+console.log("Number of seasons: " + seasons.length);
+console.log(seasons);
+
+for (const season of seasons) {
+  const startDate = season.startDate || "No Start Date";
+  const endDate = season.endDate || "No End Date";
+  console.log(`Season Start Date: ${startDate}, End Date: ${endDate}`);
+}
+
+const startDate = seasons[0].startDate;
+const endDate = seasons[0].endDate;
+
+const daysOfWeek = seasons[0].pricingRecords[0].daysOfWeek;
+console.log(daysOfWeek);
+
+const pricingRecords = seasons[0].pricingRecords;
+console.log(pricingRecords);
+let timedEntries = seasons[0].pricingRecords[0].timedEntries;
+
+if (timedEntries === undefined) {
+  timedEntries = "No Timed Entries";
+} else {
+  const numberOfTimedEntries = timedEntries.length;
+  console.log(`Number of Timed Entries: ${numberOfTimedEntries}`);
+}
+console.log(timedEntries);
+
+let unavailableDates = seasons[0].pricingRecords[0].unavailableDates;
+
+if (unavailableDates === undefined) {
+  unavailableDates = "No Unavailable Dates";
+} else {
+  const numberOfunavailableDates = unavailableDates.length;
+  console.log(`Number of Unavailable Dates: ${numberOfunavailableDates}`);
+}
+console.log(unavailableDates);
+
+//set empty variables
+let availableDates = [];
+let filteredSeasonDates = [];
+let filteredAvailableDates = [];
+let availableDays = [];
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const firstDate = new Date(); // gets the current date and time
 const lastDate = new Date();
 lastDate.setFullYear(firstDate.getFullYear() + 1); // Set the end date to one year from the current date
@@ -73,3 +126,255 @@ function filterDatesByDayOfWeek(filteredSeasonDates, daysOfWeek) {
   console.log("daysfil", filteredAvailableDates);
   return filteredAvailableDates;
 }
+//+++++++++++++++++++
+if (productOptionCode) {
+  console.log("Has PC");
+  if (Array.isArray(productOptionCode) && productOptionCode.length > 1) {
+    console.log("PC > 1");
+  } else {
+    console.log("1 PC");
+
+    if (startDate && endDate) {
+      console.log("S&E Date");
+      if (Array.isArray(daysOfWeek) && daysOfWeek.length === 7) {
+        console.log("Runs Every Day");
+        if (Array.isArray(timedEntries) && timedEntries.length > 0) {
+          console.log("Has TE");
+          let hasUnavailableDates = timedEntries.some((entry) => {
+            return (
+              Array.isArray(entry.unavailableDates) &&
+              entry.unavailableDates.length > 0
+            );
+          });
+          if (hasUnavailableDates) {
+            console.log("Has UnDates");
+            // Filter UnDates that are unavailable for all start times
+            unavailableDates = availableDates.filter((date) => {
+              return data.bookableItems[0].seasons[0].pricingRecords[0].timedEntries.every(
+                (entry) => {
+                  return entry.unavailableDates.some(
+                    (dateEntry) => dateEntry.date === date
+                  );
+                }
+              );
+            });
+            console.log("unDates", unavailableDates);
+            // Filter UnDates that are unavailable for all start times
+            const unavailableDateSet = new Set(unavailableDates);
+            filteredAvailableDates = filteredSeasonDates.filter(
+              (date) => !unavailableDateSet.has(date)
+            );
+            console.log("Fil Dates", filteredAvailableDates);
+          } else {
+            console.log("No UnDates");
+          } //Has No Unavailable Dates
+        } else {
+          console.log("No TE2");
+          let hasUnavailableDates = pricingRecords.some((entry) => {
+            return (
+              Array.isArray(entry.unavailableDates) &&
+              entry.unavailableDates.length > 0
+            );
+          });
+          if (hasUnavailableDates) {
+            console.log("Has UnDates");
+            // Filter UnDates that are unavailable for all start times
+            unavailableDates = availableDates.filter((date) => {
+              return data.bookableItems[0].seasons[0].pricingRecords[0].unavailableDates.every(
+                (entry) => {
+                  return entry.unavailableDates.some(
+                    (dateEntry) => dateEntry.date === date
+                  );
+                }
+              );
+            });
+            console.log("unDates", unavailableDates);
+            // Filter UnDates that are unavailable for all start times
+            const unavailableDateSet = new Set(unavailableDates);
+            filteredAvailableDates = filteredSeasonDates.filter(
+              (date) => !unavailableDateSet.has(date)
+            );
+            console.log("Fil Dates", filteredAvailableDates);
+          } else {
+            console.log("No UnDates");
+          }
+        } //Has No Timed Entries
+      } else {
+        console.log("Runs on: " + daysOfWeek.join(", "));
+      } //else Runs on the following days
+    } else if (startDate) {
+      console.log("Start Date");
+      if (Array.isArray(daysOfWeek) && daysOfWeek.length === 7) {
+        console.log("Runs Every Day");
+        if (Array.isArray(timedEntries) && timedEntries.length > 0) {
+          console.log("Has TE");
+          let hasUnavailableDates = timedEntries.some((entry) => {
+            return (
+              Array.isArray(entry.unavailableDates) &&
+              entry.unavailableDates.length > 0
+            );
+          });
+          if (hasUnavailableDates) {
+            console.log("Has UnDates");
+            //filterAvDatesByUnDates func
+            unavailableDates = availableDates.filter((date) => {
+              return timedEntries.every((entry) => {
+                return entry.unavailableDates.some(
+                  (dateEntry) => dateEntry.date === date
+                );
+              });
+            });
+            console.log("unDates", unavailableDates);
+            //filterAvDatesByUnDates func
+            filteredAvailableDates = filterAvDatesByUnDates(
+              filteredSeasonDates,
+              unavailableDates
+            );
+          } else {
+            console.log("No Unates");
+          } //Has No Unavailable Dates
+        } else {
+          console.log("No TE");
+          let hasUnavailableDates = pricingRecords.some((entry) => {
+            return (
+              Array.isArray(entry.unavailableDates) &&
+              entry.unavailableDates.length > 0
+            );
+          });
+
+          if (hasUnavailableDates) {
+            console.log("Has UnDates");
+
+            // Filter UnDates that are unavailable for all start times
+            unavailableDates = availableDates.filter((date) => {
+              return data.bookableItems[0].seasons[0].pricingRecords.every(
+                (entry) => {
+                  return entry.unavailableDates.some(
+                    (dateEntry) => dateEntry.date === date
+                  );
+                }
+              );
+            });
+            console.log("unDates", unavailableDates);
+            // filterAvDatesByUnDates func
+            filteredAvailableDates = filterAvDatesByUnDates(
+              filteredSeasonDates,
+              unavailableDates
+            );
+          } else {
+            console.log("No UnDates");
+          } //Has No UnDates
+        } //Has No Timed Entries
+      } else {
+        console.log("Runs on: " + daysOfWeek.join(", "));
+        filteredAvailableDates = filterDatesByDayOfWeek(
+          filteredSeasonDates,
+          daysOfWeek
+        );
+      } //else Runs on the following days
+    } else {
+      console.log("No Start Date");
+    } //else has no start date
+  } // 1 PC
+} else {
+  console.log("No PC");
+  if (startDate && endDate) {
+    console.log("Has S&E Date");
+    if (Array.isArray(daysOfWeek) && daysOfWeek.length === 7) {
+      console.log("Every Day");
+      if (Array.isArray(timedEntries) && timedEntries.length > 0) {
+        console.log("Has TE");
+        let hasUnavailableDates = timedEntries.some((entry) => {
+          return (
+            Array.isArray(entry.unavailableDates) &&
+            entry.unavailableDates.length > 0
+          );
+        });
+        if (hasUnavailableDates) {
+          console.log("Has UnDates");
+          // Filter UnDates that are un for all start times
+          unavailableDates = availableDates.filter((date) => {
+            return timedEntries.every((entry) => {
+              return entry.unavailableDates.some(
+                (dateEntry) => dateEntry.date === date
+              );
+            });
+          });
+          console.log("unDates", unavailableDates);
+          // Filter UnDates that are un for all start times
+          const unavailableDateSet = new Set(unavailableDates);
+          filteredAvailableDates = filteredSeasonDates.filter(
+            (date) => !unavailableDateSet.has(date)
+          );
+          console.log("Fil Dates", filteredAvailableDates);
+        } else {
+          console.log("No UnDates");
+        } //Has No Unavailable Dates
+      } else {
+        console.log("No TE");
+        let hasUnavailableDates = pricingRecords.some((entry) => {
+          return (
+            Array.isArray(entry.unavailableDates) &&
+            entry.unavailableDates.length > 0
+          );
+        });
+
+        if (hasUnavailableDates) {
+          console.log("Has UnDates");
+
+          // Filter UnDates that are un for all start times
+          unavailableDates = availableDates.filter((date) => {
+            return data.bookableItems[0].seasons[0].pricingRecords.every(
+              (entry) => {
+                return entry.unavailableDates.some(
+                  (dateEntry) => dateEntry.date === date
+                );
+              }
+            );
+          });
+          console.log("unDates", unavailableDates);
+          //filterAvDatesByUnDates func
+          filteredAvailableDates = filterAvDatesByUnDates(
+            filteredSeasonDates,
+            unavailableDates
+          );
+        } else {
+          console.log("No UnDates");
+        } //No Unavailable Dates
+      } //No Timed Entries
+    } else {
+      console.log("Runs on: " + daysOfWeek.join(", "));
+    } //following days
+  } else if (startDate) {
+    console.log("Has S Date");
+  } else {
+    console.log("No S Date");
+  } //no start date
+} //if product option code
+//++++++++++++++++++++++
+$("#datepicker").datepicker({
+  minDate: new Date(),
+  dateFormat: "yy-mm-dd",
+  beforeShow: function (input, inst) {
+    var currentDate = new Date();
+    var maxDate = new Date();
+    maxDate.setFullYear(currentDate.getFullYear() + 1); // Set max date
+    inst.settings.maxDate = maxDate; // Update maxDate
+  },
+  onSelect: function (dateText) {
+    $("#selected_date").val(dateText);
+    localStorage.setItem("currentDate", dateText);
+    $(".button_options.next").attr("disabled", false);
+    console.log(dateText);
+  },
+  beforeShowDay: function (date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    const formattedDate = year + "-" + month + "-" + day;
+
+    let isAvailable = filteredAvailableDates.includes(formattedDate);
+
+    return [isAvailable, isAvailable ? "" : "unavailable-date", ""];
+  },
+});
