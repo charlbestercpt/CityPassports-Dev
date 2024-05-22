d6a55bbc6ea53e5d8126600f5dc3caa73b330a41
charlbestercpt <111444412+charlbestercpt@users.noreply.github.com>
2024-05-22 11:05:04 +0200

    Missve Update

--- missive-chat-digest/server.js
+++ missive-chat-digest/server.js
@@ -1,8 +1,9 @@
 const crypto = require("crypto");
+const email = localStorage.getItem("_ms-mem").auth.email;
 
 // Replace with your actual secret key and user email
 const secretKey = "ca2887525e1ffe209063b4e690389ab1";
-const userEmail = "jane@acme.com";
+const userEmail = email;
 
 // Generate the digest
 const digest = crypto
